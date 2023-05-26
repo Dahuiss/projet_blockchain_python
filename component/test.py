51ff9297bc57f7ca03635eb7424637a77fc8ddf2
Sourcery AI <>
2023-05-26 13:58:56 +0000

    'Refactored by Sourcery'

--- component/test.py
+++ component/test.py
@@ -1,3 +1,3 @@
 import crypto_component
-print("version: " + crypto_component.getVersion())
-print("message du composant: " + crypto_component.greet())
+print(f"version: {crypto_component.getVersion()}")
+print(f"message du composant: {crypto_component.greet()}")
